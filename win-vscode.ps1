44e3b1c2f050f71e1e13ab3cdf68f6a8870188b4
hve <hve4638@gmail.com>
2025-07-27 18:47:16 +0900

    CHORE

--- win-vscode.ps1
+++ win-vscode.ps1
@@ -1,19 +1,13 @@
 Get-Desktop 0 | Switch-Desktop
-
 code ".\packages\core"
-
 Start-Sleep -Seconds 3
 
 Get-Desktop 1 | Switch-Desktop
-
 code ".\packages\front"
-
 Start-Sleep -Seconds 3
 
 Get-Desktop 2 | Switch-Desktop
-
 code ".\packages\electron"
-
 Start-Sleep -Seconds 3
 
 Get-Desktop 0 | Switch-Desktop
