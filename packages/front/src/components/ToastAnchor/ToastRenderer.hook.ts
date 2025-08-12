331809203a91c0509c57b2d1e673974faee7fb0a
hve <hve4638@gmail.com>
2025-08-12 18:48:52 +0900

    FIX: Toast 기능 수정

--- packages/front/src/components/ToastAnchor/ToastRenderer.hook.ts
+++ packages/front/src/components/ToastAnchor/ToastRenderer.hook.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useEvent } from '@/hooks/useEvent';
 import { Toast } from '@/types/toast';
@@ -6,7 +6,18 @@ import { Toast } from '@/types/toast';
 type ToastWithId = Toast & { id: number };
 function useToastRenderer() {
     const countRef = useRef(0);
+    const timeoutRef = useRef<(NodeJS.Timeout | null)[]>([]);
     const [toasts, setToasts] = useState<ToastWithId[]>([]);
+
+    useEffect(() => {
+        return () => {
+            for (const to of timeoutRef.current) {
+                if (to) {
+                    clearTimeout(to);
+                }
+            }
+        }
+    }, [])
 
     const addToast = (entry: Toast) => {
         const id = countRef.current++;
@@ -17,6 +28,11 @@ function useToastRenderer() {
             },
             ...prev,
         ]);
+        
+        const timeout = setTimeout(() => {
+            removeToast(id);
+        }, 3000);
+        timeoutRef.current.push(timeout);
     }
 
     useEvent('show_toast_message', (toast) => {
