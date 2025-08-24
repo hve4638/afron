f1575f044bdaab7a4b6e8b5ca51e7b3ee5da1563
hve <hve4638@gmail.com>
2025-08-25 01:54:29 +0900

    fix: PromptEditor hook 분리

--- packages/front/src/pages/PromptEditor/utils.ts
+++ packages/front/src/pages/PromptEditor/utils.ts
@@ -1,11 +1,15 @@
+import type {
+    PromptEditorData,
+    PromptInputType
+} from '@/types';
 
 /**
  * PromptVar의 default_value를 type에 맞게 수정
  * 
  * @param promptVar 
  */
-export function fixPromptVar(promptVar:PromptVar) {
-    switch(promptVar.type) {
+export function fixPromptVar(promptVar: PromptVar) {
+    switch (promptVar.type) {
         case 'text':
             if (typeof promptVar.default_value !== 'string') {
                 delete promptVar.default_value;
@@ -35,5 +39,41 @@ export function fixPromptVar(promptVar:PromptVar) {
                 fixPromptVar(field);
             }
             break;
+    }
+}
+
+export function getDefaultPromptEditorData(): PromptEditorData {
+    return {
+        rtId: '',
+        promptId: '',
+
+        name: null,
+        version: '1.0.0',
+        model: {
+            stream: false,
+            temperature: 1.0,
+            top_p: 1.0,
+            max_tokens: 1024,
+            use_thinking: false,
+            thinking_tokens: 1024,
+
+            safety_settings: {
+                HARM_CATEGORY_CIVIC_INTEGRITY: 'OFF',
+                HARM_CATEGORY_DANGEROUS_CONTENT: 'OFF',
+                HARM_CATEGORY_HARASSMENT: 'OFF',
+                HARM_CATEGORY_HATE_SPEECH: 'OFF',
+                HARM_CATEGORY_SEXUALLY_EXPLICIT: 'OFF',
+            },
+        },
+        variables: [],
+        changedVariables: [],
+        removedVariables: [],
+        contents: '',
+        config: {
+            inputType: 'normal',
+        },
+
+        changed: {},
+        flags: {},
     }
 }
