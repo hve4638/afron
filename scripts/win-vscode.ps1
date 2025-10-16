e0c1be45e7074a11af52ec7d19f007b08205a220
hve <hve4638@gmail.com>
2025-10-16 19:56:08 +0900

    chore

--- scripts/win-vscode.ps1
+++ scripts/win-vscode.ps1
@@ -10,13 +10,13 @@ Get-Desktop 1 | Switch-Desktop
 code ".\packages\front"
 Start-Sleep -Seconds 3
 
-Get-Desktop 2 | Switch-Desktop
-code ".\packages\core"
-Start-Sleep -Seconds 3
+# Get-Desktop 2 | Switch-Desktop
+# code ".\packages\core"
+# Start-Sleep -Seconds 3
 
-Get-Desktop 3 | Switch-Desktop
-code ".\packages\electron"
-Start-Sleep -Seconds 3
+# Get-Desktop 3 | Switch-Desktop
+# code ".\packages\electron"
+# Start-Sleep -Seconds 3
 
 
 Get-Desktop 0 | Switch-Desktop
