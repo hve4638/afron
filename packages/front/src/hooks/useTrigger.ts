4a92887a19a893c05217936c877f72d820bdf7e5
hve <hve4638@gmail.com>
2025-12-14 09:22:17 +0900

    fix

--- packages/front/src/hooks/useTrigger.ts
+++ packages/front/src/hooks/useTrigger.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 /**
  * 단순 리렌더링을 위한 훅, useRef 등의 값을 수정하며 반영이 필요한 경우 사용함
  */
-function useRerender() {
+export function useRerender() {
     const [ping, setPing] = useState<number>(0);
 
     return [
