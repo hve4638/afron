adb211eade07b944467704e8ad2f159893a4ec14
hve <hve4638@gmail.com>
2026-01-09 19:18:06 +0900

    chore

--- scripts/win-vscode.ps1
+++ scripts/win-vscode.ps1
@@ -7,11 +7,11 @@ code ".\"
 Start-Sleep -Seconds 3
 
 Get-Desktop 1 | Switch-Desktop
-code ".\packages\core"
+code ".\packages\front"
 Start-Sleep -Seconds 3
 
 Get-Desktop 2 | Switch-Desktop
-code ".\packages\front"
+code ".\packages\core"
 Start-Sleep -Seconds 3
 
 Get-Desktop 0 | Switch-Desktop
